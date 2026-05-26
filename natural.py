ee629f6498d1e06338f15515eea2d889a58a13ac
IHOTU89 <amicable269@gmail.com>
2026-05-26 10:37:28 +0100

    Add files via upload

---
+++ natural.py
@@ -0,0 +1,8 @@
+number=int(input("Enter number to check"))
+print("Number to be checked:",number)
+
+if number%2==0:
+   print ("this is a natural number")
+
+else:
+   print("this is an even number")
